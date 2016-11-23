b5059482327afc97e61c5b9bb1d581160c24b517
Rails Git setup <aw651834@sju.edu>
2016-11-23 17:47:55 +0000

    Add edit,update,sho and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,18 @@
 class ArticlesController < ApplicationController
     
+    def index
+        #Grab all the articles from the database
+        @articles = Article.all
+        
+    end
+    
     def new
         @article = Article.new
+        
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
         
     end
     
@@ -22,6 +33,18 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+        
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        
+            if @article.update(article_params)            
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+            else
+            render 'edit'
+            end
         
     end
     
