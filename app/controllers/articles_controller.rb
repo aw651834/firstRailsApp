db1e3b38cc998924b7b91f6220fc9f18d55c203e
Rails Git setup <aw651834@sju.edu>
2016-11-23 16:34:12 +0000

    Fixed the create,add flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,22 @@ class ArticlesController < ApplicationController
         
         #create an instance variable
         @article = Article.new(article_params)
-        @article.save
+        #@article.save
         
         #page to redirect to after saving
-        redirect_to articles_show(@article)
+        #redirect_to article_path(@article)
         
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+        
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private 
