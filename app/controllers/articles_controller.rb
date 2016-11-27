e3c7e92f7448fea9cfb98337a3d76c58488c413c
Rails Git setup <aw651834@sju.edu>
2016-11-27 00:27:09 +0000

    Added styling to error and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
         #redirect_to article_path(@article)
         
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
         
         
             if @article.update(article_params)            
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article)
             else
             render 'edit'
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
         
         @article.destroy
         
-        flash[:notice] = "Article was successfully deleted"
+        flash[:success] = "Article was successfully deleted"
         redirect_to articles_path
     end
     
